9c056db85e0f00e083ff4679bbbf95c7b1120c8b
Jorge Sepulveda <gSepulveda9697@gmail.com>
2020-05-01 13:07:30 -0500

    added popup functionality

--- src/scripts.js
+++ src/scripts.js
@@ -3,7 +3,7 @@ $.getJSON("https://raw.githubusercontent.com/jorge-sepulveda/covid-time-map/mast
 	data = json;
 });
 
-currentDateSelected = '04-28-2020'
+var currentDateSelected = '04-30-2020'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ3NlcHVsdmVkYTk2IiwiYSI6ImNrNzlmaGFvNDBzcHozZG9kOXQxNjF0bW8ifQ.P4nx2cJHuZTio2JivJyBDA';
 var map = new mapboxgl.Map({
@@ -12,7 +12,6 @@ var map = new mapboxgl.Map({
 	center: [-99.9, 41.5],
 	zoom: 4
 });
-var hoveredStateId = null;
 
 var popup = new mapboxgl.Popup({
 	closeButton: false
@@ -20,10 +19,10 @@ var popup = new mapboxgl.Popup({
 
 function validateDate() {
 	var minDate = new Date('03/21/2020');
-	var maxDate = new Date('04/30/2020');
+	var maxDate = new Date('05/01/2020');
 	var dateToCheck = new Date($("#mapdate").val())
 
-	if (dateToCheck > minDate && dateToCheck <= maxDate) {
+	if (dateToCheck > minDate && dateToCheck < maxDate) {
 		reloadMap()
 	} else {
 		alert('Selected date out of range or duplicate!!' + dateToCheck + '')
@@ -31,7 +30,6 @@ function validateDate() {
 }
 
 function reloadMap() {
-	console.log("reload map called");
 	dateValue = moment($("#mapdate").val());
 	dateToLoad = dateValue.format("MM-DD-YYYY").toString()
 	console.log(dateToLoad)
@@ -46,16 +44,12 @@ function reloadMap() {
 
 	data[currentDateSelected].forEach(function (row) {
 		number = (row['infection_rate'])
-		//var green = (row['infection_rate'] / 9967 ) * 255;
-		//var color = 'rgba(' + 0 + ', ' + green + ', ' + 0 + ', 1)';
-
 		var color = (number > 1000) ? "#AE8080" :
 			(number > 500) ? "#D18080" :
 			(number > 100) ? "#FC8B8B" :
 			(number > 10) ? "#FDC4C4" :
 			(number > 1) ? "#FDE6E6" :
 			"#FFFFFF";
-
 		newExpression.push(row['fips'], color);
 	});
 
@@ -113,32 +107,29 @@ map.on('load', function () {
 	});
 
 	map.on('mousemove', 'states-join', function (e) {
-
-		/*if (e.features.length > 0) {
-		    if (hoveredStateId) {
-		    map.setFeatureState(
-		    { source: 'counties-with-pops-f-8nbien', sourceLayer: 'counties-with-pops-f-8nbien', id: hoveredStateId },
-		    { hover: false }
-		    );
-		    }
-		    hoveredStateId = e.features[0].id;
-		    map.setFeatureState(
-		    { source: 'counties-with-pops-f-8nbien', sourceLayer: 'counties-with-pops-f-8nbien', id: hoveredStateId },
-		    { hover: true }
-		    );
-		    }*/
-		// Change the cursor style as a UI indicator.
-
+		map.getCanvas().style.cursor = 'pointer';
+		var coordinates = e.features[0].geometry.coordinates.slice();
+		var description = e.features[0].properties.description;
 		// Single out the first found feature.
+		while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+			coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+			}
 		var feature = e.features[0];
-		//console.log(feature)
+		selectedCounty = data[currentDateSelected].filter(county => county.fips === feature.properties.fips);
 
-		// Display a popup with the name of the county
-		/*popup.setLngLat(e.lngLat)
+		// Display a popup with the name of the county and info
+		popup.setLngLat(e.lngLat)
 			.setHTML('County: ' + feature.properties.NAME + '</br>' 
-					+'Population: ' + feature.properties.POPESTIMATE2019 + "</br>")
-            .addTo(map);*/
+					+'Population: ' + feature.properties.POPESTIMATE2019 + '</br>'
+					+'Cases: ' + selectedCounty[0]['confirmed'] + '</br>'
+					+'Infection Rate: ' + selectedCounty[0]['infection_rate'].toFixed(2) + '/100,000 People</br>'
+					+'Recovered: ' + selectedCounty[0]['recovered'] + '</br>'
+					+'Active: ' + selectedCounty[0]['active'] + '</br>'
+					+'Deaths: ' + selectedCounty[0]['confirmed'] + '</br>')
+            .addTo(map);
 	});
-
-
+	map.on('mouseleave', 'places', function() {
+		map.getCanvas().style.cursor = '';
+		popup.remove();
+	});
 });
